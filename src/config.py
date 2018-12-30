26fe8687d6f34e7d796ddf4c2e5fa5c5320aab4d
Ilia Bozhinov <ammen99@gmail.com>
2018-12-30 23:49:10 +0200

    fixup! add config file

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Attributes of the server this bot will run on
+SERVER_HOST = ''
+SERVER_PORT = 8888
+
+# Attributes of the IRC connection
+IRC_SERVER = 'chat.freenode.net'
+IRC_CHANNEL = '#wayfire'
+IRC_NICK = 'WfNotifyBot'
+IRC_PORT = 6667
