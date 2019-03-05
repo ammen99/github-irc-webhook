dc30359c715746578a192d6d9b0bb9ea02d5bd29
Ilia Bozhinov <ammen99@gmail.com>
2019-03-05 16:05:57 +0100

    mention freenode specific password requirements in the config file

--- src/config.py
+++ src/config.py
@@ -6,5 +6,10 @@ SERVER_PORT = 8888
 IRC_SERVER = 'chat.freenode.net'
 IRC_CHANNEL = '#wfbottest'
 IRC_NICK = 'WfTestBot'
-IRC_PASS = '' # Leave empty if you do not want to use registered nickname
+
+# Set the password for your registered empty, leave empty if not applicable
+# Note: freenode(and potentially other servers) want password to be of the form
+# "nick:pass", so for ex. IRC_PASS = 'WfTestBot:mypass123'
+IRC_PASS = ''
+
 IRC_PORT = 6667
